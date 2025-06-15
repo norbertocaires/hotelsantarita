246258f4e396860de24d701165531dd68fa0c4b3
norbertocaires <beto_caires@hotmail.com>
2025-06-15 20:46:47 -0300

    Pequenos ajustes

--- src/App.js
+++ src/App.js
@@ -20,17 +20,17 @@ function App() {
           </div>
         </div>
 
-        <iframe 
-          title="Localização Hotel Santa Rita - Mapa principal"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2781.9005822297536!2d-41.328532726663!3d-13.627629973343018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x743fcee95d2e059%3A0xe07356b8e0d45b8c!2sHotel%20Santa%20Rita!5e1!3m2!1spt-BR!2sbr!4v1750030396057!5m2!1spt-BR!2sbr" 
-          width="400" 
-          height="300" 
-          style={{ border: 0 }} 
-          allowFullScreen 
-          loading="lazy" 
-          referrerPolicy="no-referrer-when-downgrade">
-        </iframe>
-        
+        <iframe
+          title="Localização Hotel Santa Rita"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15509.935529230977!2d-41.330753699999995!3d-13.6283037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x743fcee95d2e059%3A0xe07356b8e0d45b8c!2sHotel%20Santa%20Rita!5e0!3m2!1spt-BR!2sbr!4v1748729534447!5m2!1spt-BR!2sbr"
+          width="100%"
+          height="300"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+
       </section>
 
       <section className="info-box">
